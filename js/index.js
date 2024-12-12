c37fc1fc09cf5ed9cc746a7517475f62357a1258
Sultan Bakhytzhanov <bahytzanovsultan@gmail.com>
2024-12-12 23:03:28 +0500

    whatever commit

--- js/index.js
+++ js/index.js
@@ -2,3 +2,5 @@ window.addEventListener('scroll', function () {
 	let nav = document.querySelector('nav');
 	nav.classList.toggle('sticky', window.scrollY > 0);
 });
+
+// Посмотреть сайт академии Шаг
